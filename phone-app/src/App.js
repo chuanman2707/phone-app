531929b7ac77765dc19fe86ee25ebfcca3820343
chuanman2707 <chuanman2707@gmail.com>
2019-05-17 21:10:06 +0700

    update

--- phone-app/src/App.js
+++ phone-app/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,18 +8,21 @@ import Details from "./components/Details";
 import ParticleContainer from "./components/ParticleContainer";
 import Default from "./components/Default";
 import ProductList from "./components/ProductList";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <>
       <Navbar />
-      <ParticleContainer />
+      {/* <ParticleContainer /> */}
+
       <Switch>
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
         <Route path="/cart" component={Cart} />
         <Route component={Default} />
       </Switch>
+      <Modal />
     </>
   );
 }
